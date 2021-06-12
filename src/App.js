84cad87d678d97f4a322d973ae21343e62cc8e72
rajveer singh <rajveermbic@gmail.com>
2021-06-13 00:15:28 +0530

    componenets created

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import {React} from 'react'
-import Navbar from './components/navbar';
-import Header from './components/header';
-import AddNotes from './form-add-notes';
-import CompletedTask from './components/sec-completed-task';
-import IncompleteTask from './components/incompleted-task';
+import Main from './components/main';
+
 function App() {
   return (
-    <div className="main">
-      <Navbar/>
-      <Header/>
-      {/* The input shows to add notes */}
-      <AddNotes/>
-      {/* Rest of the container items except nvabar */}
-      <div class="container ">
-        <div class="row mx-auto my-8">
-          {/* Section for completed tasks  */}
-          <CompletedTask/>
+       <>
+       <Main/>
+       </>
 
-          {/*  Incompleted tasks */}
-          <IncompleteTask/>
-        </div>
-      </div>
-    </div>
   );
+    
 }
 
 export default App;
