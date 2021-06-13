4cdf6ea77348ae0753e1d1d793d2826063491cb6
swayam-coder <swayam14feb@gmail.com>
2021-06-13 23:06:12 +0530

    Added edit feature

--- src/components/form-add-notes.js
+++ src/components/form-add-notes.js
@@ -1,24 +1,25 @@
-import React,{useState} from 'react'
+import React from 'react'
 
 const AddNotes = (props) => {
 const {todo,handleChange,handleSubmit}=props
  return (
-   <div className='addNotes '>
+   <div className='addNotes'>
      <div class='input-group mx-auto mb-3 w-50'>
+     <form onSubmit={handleSubmit}>
        <input
          type='text'
-         class='form-control'
+         className='form-control'
          placeholder='Add your task here...'
          aria-label='Add your task here...'
-         aria-describedby='button-addon2'
-         value={todo}
+         value={todo.task}
          onChange={handleChange}
        />
        <div class='input-group-append'>
-         <button class='btn btn-primary' type='button' id='button-addon2' onClick={handleSubmit}>
+         <button class='btn btn-primary' type='submit' id='button-addon2'>
            Add
          </button>
        </div>
+       </form>
      </div>
    </div>
  )
